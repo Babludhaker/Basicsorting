2bc2be40421a2b681d81fbe0beead55e414f8966
Babludhaker <babludhaker328@gmail.com>
2022-08-20 22:08:15 +0530

    update

--- Sorting.java
+++ Sorting.java
@@ -51,9 +51,31 @@ public static void insertionSort(int arr[]){
     }
 }
 
+//Counting Sort
+public static void countingSort(int arr[]){
+    // check largest element
+    int largest = Integer.MIN_VALUE;
+    for(int i=0; i<arr.length; i++){
+        largest = Math.max(largest, arr[i]);
+    }
+    //create a count array
+    int count[] = new int[largest+1];
+    for(int i=0; i<arr.length; i++){
+        count[arr[i]]++;
+    }
+    //sorting
+    int j = 0;
+    for(int i=0; i<count.length; i++){
+        while(count[i]>0){
+            arr[j] = i;
+            j++;
+            count[i]--;
+        }
+    }
+}
     public static void main(String args[]) {
-        int arr[] = {5,4,1,3,2,6};
-       insertionSort(arr);
-        print(arr);
+        int arr[] = {1,4,1,3,2,4,3,7};
+countingSort(arr);    
+    print(arr);
     }
 }

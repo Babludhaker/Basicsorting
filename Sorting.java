d6337c772173b080c429e4f69a159583166cd995
Babludhaker <babludhaker328@gmail.com>
2022-08-20 08:13:35 +0530

    sortingArray

---
+++ Sorting.java
@@ -0,0 +1,48 @@
+public class Sorting {
+    // bubble short
+    public static void sorting(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                   
+                    
+                }
+            }
+            
+           
+        }
+        
+    }
+    
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    //Selection Sorting
+    public static void selctionSort(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            int minPos = i;
+            for(int j=i+1;j<arr.length; j++){
+                if(arr[minPos]> arr[j]){
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {5,4,1,3,2,6};
+        selctionSort(arr);
+        //print(arr);
+    }
+}

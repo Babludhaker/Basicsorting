7679e4125a27210eadf85038646c5b3a458b6398
Babludhaker <babludhaker328@gmail.com>
2022-08-20 10:56:49 +0530

    modified

--- Sorting.java
+++ Sorting.java
@@ -38,9 +38,22 @@ public class Sorting {
         }
     }
 
+    // Insertion Sorting
+public static void insertionSort(int arr[]){
+    for(int i=1; i<arr.length; i++){
+        int temp = arr[i];
+         int prev = i-1; // prev yani pahle wala element
+         while(prev>=0 && arr[prev] > temp){
+            arr[prev+1] = arr[prev];
+            prev--;
+         }
+         arr[prev+1] = temp;// kyuki prev ki value -1 hogi tab temp ka value assign
+    }
+}
+
     public static void main(String args[]) {
         int arr[] = {5,4,1,3,2,6};
-        selctionSort(arr);
+       insertionSort(arr);
         print(arr);
     }
 }
